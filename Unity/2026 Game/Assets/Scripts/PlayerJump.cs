498e4ebc04fc8eecae91fb458f3591bb88e673ac
Lindsey Welton <10878168@uvu.edu>
2026-01-27 15:22:04 -0700

    Goblin Game
    - Raycasting (it's in the sky)

--- Unity/2026 Game/Assets/Scripts/PlayerJump.cs
+++ Unity/2026 Game/Assets/Scripts/PlayerJump.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidBody2D;
+    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private float jumpForce = 6;
+    private float playerHalfHeight;
+
+    private void Start()
+    {
+        playerHalfHeight = spriteRenderer.bounds.extents.y;
+    }
+    
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Space))
+        
+        Debug.DrawRay(transform.position, Vector2.down * (playerHalfHeight + 0.1f), Color.red);
+        
+       if (Input.GetKeyDown(KeyCode.Space) && GetIsGrounded())
         {
             Jump();
         }
+    }
+
+    private bool GetIsGrounded()
+    {
+       return Physics2D.Raycast(transform.position, Vector2.down, playerHalfHeight + 0.1f, LayerMask.GetMask("Ground")); 
     }
     private void Jump()
     {
