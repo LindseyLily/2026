738048bef4298132bdeb07e8acaeb6098b4ce072
Lindsey Welton <10878168@uvu.edu>
2026-01-27 14:26:22 -0700

    Goblin Game
    - rewriting the character controller to work with a 2D rigid body

--- Unity/2026 Game/Assets/Scripts/LilGuyController.cs
+++ Unity/2026 Game/Assets/Scripts/LilGuyController.cs
@@ -1,43 +1,19 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
-
-[RequireComponent(typeof(CharacterController))]
-    
 public class LilGuyController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    
-    private CharacterController controller;
-    private Vector3 velocity;
-    private Transform thisTransform;
-
-    private void Start()
-    {
-        controller = GetComponent<CharacterController>();
-        thisTransform = transform;
-    }
+    public float speed = 5f;
+    private Vector2 movement;
 
     private void Update()
     {
-        MoveCharacter();
-        KeepCharacterOnxAxis();
-    }
-
-    private void MoveCharacter()
-    {
-        //Horizontal movement
-        var moveInput = Input.GetAxis("Horizontal");
-        var move = new Vector3(x: moveInput, y: 0f, z: 0f) * (moveSpeed * Time.deltaTime);
-        controller.Move(move);
-    }
-
-    private void KeepCharacterOnxAxis()
-    {
-        // Prevents character from moving on the z axis
-        var currentPosition = thisTransform.position;
-        currentPosition.z = 0f;
-        thisTransform.position = currentPosition;
+        // GetAxisRaw() is exactly -1 or +1
+        // GetAxis() uses the decimals in between (Good for acceleration)
+        // Mapping the axis for keycodes and buttons
+        float input = Input.GetAxis("Horizontal");
+        movement.x = input * speed * Time.deltaTime;
+        transform.Translate(movement);
     }
 }
-
